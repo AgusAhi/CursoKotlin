5ab110e8295a180549021e1fc496d37dcf2e287f
AgusAhi <aguahidol@alu.edu.gva.es>
2024-10-21 17:16:52 +0200

    yey

--- app/src/main/java/com/example/cursokotlin/Units/Unit8/Exercise23.kt
+++ app/src/main/java/com/example/cursokotlin/Units/Unit8/Exercise23.kt
@@ -14,7 +14,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun Project23(modifier: Modifier = Modifier, navController: NavHostController) {
